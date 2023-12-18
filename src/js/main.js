071134b5e66e5e4be06c2ce85b57658bfceeb44c
Daniel Gonçalves <daniel.tekla123@gmail.com>
2023-12-18 20:46:17 -0300

    Organização de funções na Classe

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 var deckUser = [];
 var deckOponent = [];
 var deckGame = "";
-var totalPointsUser = 0;
-var totalPointsOponent = 0;
+var cardsUser = document.querySelector('.table_user')
+var cardsOponent = document.querySelector('.table_oponent')
 var drawNewCardUser = document.querySelector('#drawNewCardUser')
 drawNewCardUser.addEventListener('click', () => deckGame.drawNewCard(deckUser, 1))
 
@@ -14,185 +14,9 @@ async function getDeckId() {
         const res = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${deck_count}`);
         const data = await res.json();
         const deckId = await data.deck_id;
-        // console.log(deckId)
         return deckId;
     } catch (error) {
         console.error(error);
-    }
-}
-
-async function convertCardsToHtmlUser() {
-    const cardsUser = document.querySelector('.table_user')
-    cardsUser.innerHTML = '';
-
-    for (let i = 0; i < deckUser.length; i++) {
-        let card = deckUser[i];
-
-        cardsUser.innerHTML += `
-        <div>
-            <div class='cards' style="background-image: url('${card.image}')"></div>
-            <h1>${card.value}<span>${card.suit}</span></h1>
-        </div>
-        `;
-    }
-}
-
-async function convertCardsToHtmlOponent() {
-    const cardsOponent = document.querySelector('.table_oponent')
-    cardsOponent.innerHTML = '';
-    let i = 0;
-
-    for (i = 0; i < deckOponent.length; i++) {
-        let card = deckOponent[i];
-
-        if (i < 1) {
-            cardsOponent.innerHTML = `
-                <div>
-                    <div class='cards' style="background-image: url('${card.image}')"></div>
-                    <h1>${card.value}<span>${card.suit}</span></h1>
-                </div>
-                `;
-        } else {
-            cardsOponent.innerHTML += `
-                <div>
-                    <div class='back-card'></div>
-                    <h1 class='info-back-card'>${card.value}<span>${card.suit}</span></h1>
-                </div>
-                `;
-        }
-    }
-}
-
-async function revelCardsOponent() {
-    const cardsOponent = document.querySelector('.table_oponent')
-    let allCards = [];
-
-    // Revelar cartas viradas
-    for (let i = 0; i < deckOponent.length; i++) {
-        const card = deckOponent[i];
-
-        allCards += `
-            <div>
-                <div class='cards' style="background-image: url('${card.image}')"></div>
-                <h1>${card.value}<span>${card.suit}</span></h1>
-            </div>
-        `
-
-        cardsOponent.innerHTML = allCards
-    }
-}
-
-async function countPointsOponent() {
-    totalPointsOponent = 0;
-
-    for (let i = 0; i < deckOponent.length; i++) {
-        let cardValue = deckOponent[i].value;
-        revelCardsOponent();
-
-        // Contabilizar pontos Oponente
-        if (cardValue >= 1 && cardValue <= 10) {
-            const cardPoint = Number(cardValue)
-
-            switch (cardPoint) {
-                case 1:
-                    totalPointsOponent += 1;
-                    break;
-                case 2:
-                    totalPointsOponent += 2;
-                    break;
-                case 3:
-                    totalPointsOponent += 3;
-                    break;
-                case 4:
-                    totalPointsOponent += 4;
-                    break;
-                case 5:
-                    totalPointsOponent += 5;
-                    break;
-                case 6:
-                    totalPointsOponent += 6;
-                    break;
-                case 7:
-                    totalPointsOponent += 7;
-                    break;
-                case 8:
-                    totalPointsOponent += 8;
-                    break;
-                case 9:
-                    totalPointsOponent += 9;
-                    break;
-                case 10:
-                    totalPointsOponent += 10;
-                    break;
-                default:
-                    alert("Erro ao calcular pontuação final.")
-            }
-        } else if (cardValue == 'QUEEN' || cardValue == 'JACK' || cardValue == 'KING') {
-            totalPointsOponent += 10;
-        } else if (cardValue == 'ACE') {
-            if (totalPointsOponent >= 11) {
-                totalPointsOponent += 1;
-            } else {
-                totalPointsOponent += 11;
-            }
-        } else {
-            alert('Error na soma de pontos do Oponente.')
-        }
-    }
-}
-
-async function countPointsUser() {
-    totalPointsUser = 0;
-    
-    for (let i = 0; i < deckUser.length; i++) {
-        let cardValue = deckUser[i].value;
-
-        if (cardValue >= 1 && cardValue < 11) {
-            const cardPoint = Number(cardValue)
-
-            switch (cardPoint) {
-                case 1:
-                    totalPointsUser += 1;
-                    break;
-                case 2:
-                    totalPointsUser += 2;
-                    break;
-                case 3:
-                    totalPointsUser += 3;
-                    break;
-                case 4:
-                    totalPointsUser += 4;
-                    break;
-                case 5:
-                    totalPointsUser += 5;
-                    break;
-                case 6:
-                    totalPointsUser += 6;
-                    break;
-                case 7:
-                    totalPointsUser += 7;
-                    break;
-                case 8:
-                    totalPointsUser += 8;
-                    break;
-                case 9:
-                    totalPointsUser += 9;
-                    break;
-                case 10:
-                    totalPointsUser += 10;
-                    break;
-                default:
-                    alert("Erro ao calcular pontuação final.")
-            }
-        } else if (cardValue == 'QUEEN' || cardValue == 'JACK' || cardValue == 'KING') {
-            totalPointsUser += 10;
-        } else if (cardValue == 'ACE') {
-            if (confirm("O ÁS pode ter valor 1 ou 11.Para definir que ele tenha valor 1 clique 'OK', ou se quer que seu valor seja 11 clique 'Cancelar'.") == true) {
-                totalPointsUser += 1;
-            } else {
-                totalPointsUser += 11;
-            }
-        }
     }
 }
 
@@ -201,7 +25,7 @@ async function initialize() {
     const deckId = await getDeckId();
 
     // Criando a instância de Deck com deckId
-    deckGame = new Deck(deckId);
+    deckGame = new Deck(deckId, cardsUser, cardsOponent);
 
     // Comprando as duas cartas iniciais para cada lado
     deckGame.drawCardStart(deckUser, 2)
@@ -209,24 +33,3 @@ async function initialize() {
 }
 
 initialize();
-
-async function endGame() {
-    // Contagem de pontos do Usuário
-    countPointsUser();
-
-    console.log(`Total points user: ${totalPointsUser}`)
-    console.log(deckUser)
-    // Fim contagem de pontos do Usuário
-
-    // Contagem de pontos do Oponente
-    countPointsOponent();
-
-    while (totalPointsOponent <= 14) {
-        await deckGame.drawNewCard(deckOponent, 1);
-        countPointsOponent();
-    }
-
-    console.log(`Total points oponent: ${totalPointsOponent}`)
-    console.log(deckOponent)
-    // Fim contagem de pontos do Oponente
-}
--- src/models/Deck.js
+++ src/models/Deck.js
@@ -1,11 +1,83 @@
 class Deck {
     deck_id;
+    table_user;
+    table_oponent;
+    totalPointsUser;
+    totalPointsOponent;
 
-    constructor(deck_id){
+    constructor(
+        deck_id, table_user, table_oponent, totalPointsUser = 0, totalPointsOponent = 0
+        ){
         this.deck_id = deck_id;
+        this.table_user = table_user;
+        this.table_oponent = table_oponent;
+        this.totalPointsUser = totalPointsUser;
+        this.totalPointsOponent = totalPointsOponent;
     }
 
-    async drawCardStart(deck, qtde){
+    // Converte as cartas da API para elementos HTML
+    async convertCardsToHtmlUser() {
+        this.table_user.innerHTML = '';
+    
+        for (let i = 0; i < deckUser.length; i++) {
+            let card = deckUser[i];
+    
+            this.table_user.innerHTML += `
+            <div>
+                <div class='cards' style="background-image: url('${card.image}')"></div>
+                <h1>${card.value}<span>${card.suit}</span></h1>
+            </div>
+            `;
+        }
+    }
+
+    // Converte as cartas da API para elementos HTML
+    async convertCardsToHtmlOponent() {
+        this.table_oponent.innerHTML = '';
+        let i = 0;
+    
+        for (i = 0; i < deckOponent.length; i++) {
+            let card = deckOponent[i];
+    
+            if (i < 1) {
+                this.table_oponent.innerHTML = `
+                    <div>
+                        <div class='cards' style="background-image: url('${card.image}')"></div>
+                        <h1>${card.value}<span>${card.suit}</span></h1>
+                    </div>
+                    `;
+            } else {
+                this.table_oponent.innerHTML += `
+                    <div>
+                        <div class='back-card'></div>
+                        <h1 class='info-back-card'>${card.value}<span>${card.suit}</span></h1>
+                    </div>
+                    `;
+            }
+        }
+    }
+
+    // Revela as cartas viradas do Oponente
+    async revealCardsOponent() {
+        let allCards = [];
+    
+        // Revelar cartas viradas
+        for (let i = 0; i < deckOponent.length; i++) {
+            const card = deckOponent[i];
+    
+            allCards += `
+                <div>
+                    <div class='cards' style="background-image: url('${card.image}')"></div>
+                    <h1>${card.value}<span>${card.suit}</span></h1>
+                </div>
+            `
+    
+            this.table_oponent.innerHTML = allCards
+        }
+    }
+
+    // Compra de cartas iniciais
+    async drawCardStart(deck, qtde = 2){
         try {
             const res = await fetch(`https://deckofcardsapi.com/api/deck/${this.deck_id}/draw/?count=${qtde}`)
             const data = await res.json();
@@ -17,14 +89,15 @@ class Deck {
         }
 
         if(deck === deckUser){
-            convertCardsToHtmlUser();
+            this.convertCardsToHtmlUser();
         } else if (deck === deckOponent){
-            convertCardsToHtmlOponent();
+            this.convertCardsToHtmlOponent();
         } else {
             alert("Erro ao carregar as cartas.")
         }
     }
 
+    // Compra de novas cartas
     async drawNewCard(deck, qtde){
         try {
             const res = await fetch(`https://deckofcardsapi.com/api/deck/${this.deck_id}/draw/?count=${qtde}`)
@@ -36,11 +109,149 @@ class Deck {
         }
 
         if(deck === deckUser){
-            convertCardsToHtmlUser();
+            this.convertCardsToHtmlUser();
         } else if (deck === deckOponent){
-            convertCardsToHtmlOponent();
+            this.convertCardsToHtmlOponent();
         } else {
             alert("Erro ao carregar as cartas.")
         }
+    }
+
+    // Contagem final de pontos do Oponente
+    async countPointsOponent() {
+        this.totalPointsOponent = 0;
+    
+        for (let i = 0; i < deckOponent.length; i++) {
+            let cardValue = deckOponent[i].value;
+            this.revealCardsOponent();
+    
+            // Contabilizar pontos Oponente
+            if (cardValue >= 1 && cardValue <= 10) {
+                const cardPoint = Number(cardValue)
+    
+                switch (cardPoint) {
+                    case 1:
+                        this.totalPointsOponent += 1;
+                        break;
+                    case 2:
+                        this.totalPointsOponent += 2;
+                        break;
+                    case 3:
+                        this.totalPointsOponent += 3;
+                        break;
+                    case 4:
+                        this.totalPointsOponent += 4;
+                        break;
+                    case 5:
+                        this.totalPointsOponent += 5;
+                        break;
+                    case 6:
+                        this.totalPointsOponent += 6;
+                        break;
+                    case 7:
+                        this.totalPointsOponent += 7;
+                        break;
+                    case 8:
+                        this.totalPointsOponent += 8;
+                        break;
+                    case 9:
+                        this.totalPointsOponent += 9;
+                        break;
+                    case 10:
+                        this.totalPointsOponent += 10;
+                        break;
+                    default:
+                        alert("Erro ao calcular pontuação final.")
+                }
+            } else if (cardValue == 'QUEEN' || cardValue == 'JACK' || cardValue == 'KING') {
+                this.totalPointsOponent += 10;
+            } else if (cardValue == 'ACE') {
+                if (this.totalPointsOponent >= 11) {
+                    this.totalPointsOponent += 1;
+                } else {
+                    this.totalPointsOponent += 11;
+                }
+            } else {
+                alert('Error na soma de pontos do Oponente.')
+            }
+        }
+    }
+
+    // Contagem final de pontos do Usuário
+    async countPointsUser() {
+        this.totalPointsUser = 0;
+        
+        for (let i = 0; i < deckUser.length; i++) {
+            let cardValue = deckUser[i].value;
+    
+            if (cardValue >= 1 && cardValue < 11) {
+                const cardPoint = Number(cardValue)
+    
+                switch (cardPoint) {
+                    case 1:
+                        this.totalPointsUser += 1;
+                        break;
+                    case 2:
+                        this.totalPointsUser += 2;
+                        break;
+                    case 3:
+                        this.totalPointsUser += 3;
+                        break;
+                    case 4:
+                        this.totalPointsUser += 4;
+                        break;
+                    case 5:
+                        this.totalPointsUser += 5;
+                        break;
+                    case 6:
+                        this.totalPointsUser += 6;
+                        break;
+                    case 7:
+                        this.totalPointsUser += 7;
+                        break;
+                    case 8:
+                        this.totalPointsUser += 8;
+                        break;
+                    case 9:
+                        this.totalPointsUser += 9;
+                        break;
+                    case 10:
+                        this.totalPointsUser += 10;
+                        break;
+                    default:
+                        alert("Erro ao calcular pontuação final.")
+                }
+            } else if (cardValue == 'QUEEN' || cardValue == 'JACK' || cardValue == 'KING') {
+                this.totalPointsUser += 10;
+            } else if (cardValue == 'ACE') {
+                if (confirm("O ÁS pode ter valor 1 ou 11.Para definir que ele tenha valor 1 clique 'OK', ou se quer que seu valor seja 11 clique 'Cancelar'.") == true) {
+                    this.totalPointsUser += 1;
+                } else {
+                    this.totalPointsUser += 11;
+                }
+            }
+        }
+    }
+
+    // Finalização de jogo pelo usuário
+    async endGame() {
+        // Contagem de pontos do Usuário
+        deckGame.countPointsUser();
+    
+        console.log(`Total points user: ${deckGame.totalPointsUser}`)
+        console.log(deckUser)
+        // Fim contagem de pontos do Usuário
+    
+        // Contagem de pontos do Oponente
+        deckGame.countPointsOponent();
+    
+        while (deckGame.totalPointsOponent <= 14) {
+            await deckGame.drawNewCard(deckOponent, 1);
+            deckGame.countPointsOponent();
+        }
+    
+        console.log(`Total points oponent: ${deckGame.totalPointsOponent}`)
+        console.log(deckOponent)
+        // Fim contagem de pontos do Oponente
     }
 }
